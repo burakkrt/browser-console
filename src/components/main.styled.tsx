0c01b3e8b15a7ef04bf5f47f7c78c5a786963e47
burakkrt <krtburak@outlook.com>
2023-08-15 14:11:19 +0300

    last commit

--- src/components/main.styled.tsx
+++ src/components/main.styled.tsx
@@ -1,12 +1,17 @@
 import styled from 'styled-components'
 
 export const Container = styled.div<{ mode?: 'dark' | 'light' }>`
+  display: flex;
+  flex-direction: row;
+  gap: 50px;
   width: 80%;
   margin: auto;
   height: 100vh;
-  overflow-y: auto;
   padding: 10px;
   box-shadow: 0 0 8px rgba(0, 0, 0, 0.5);
   border: 2px solid #F1F0E8;
-    //border: 2px solid ${props => props?.mode === 'dark' ? 'black' : 'red'}
+`
+
+export const ContainerHalf = styled.div`
+  flex-basis: auto;
 `
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { Container } from './components/main.styled.tsx'
+import { Container, ContainerHalf } from './components/main.styled.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Container mode={'dark'}>Lorem ipsum dolor sit amssssset,ssa consectetur adipisicing elit. Assumenda cupiditate,
-      exercitationem
-      illum in
-      ipsa
-      labore modi molestiae natus nesciunt non numquam odio officiis, porro quae quos ut velit voluptas
-      voluptatum.</Container>
+    <Container mode={'dark'}>
+      <ContainerHalf>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam cumque, delectus dolorem ducimus earum neque
+        non numquam placeat praesentium qui quod reiciendis temporibus voluptates? Dolore iusto minima nam quo tempora?
+      </ContainerHalf>
+      <ContainerHalf>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem esse fugiat illum in iusto, quaerat quisquam
+        saepe tempora ut. Amet culpa minus odio perferendis quas quo repudiandae tempore velit veniam.
+      </ContainerHalf>
+    </Container>
   </React.StrictMode>,
 )
