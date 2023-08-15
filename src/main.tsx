2cb65a872c56f64f86514d060d5e5b2156df8570
burakkrt <krtburak@outlook.com>
2023-08-15 12:26:42 +0300

    project sekeleton createed

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { Container } from './components/main.styled.tsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Container mode={'dark'}>Lorem ipsum dolor sit amssssset,ssa consectetur adipisicing elit. Assumenda cupiditate,
+      exercitationem
+      illum in
+      ipsa
+      labore modi molestiae natus nesciunt non numquam odio officiis, porro quae quos ut velit voluptas
+      voluptatum.</Container>
+  </React.StrictMode>,
+)
